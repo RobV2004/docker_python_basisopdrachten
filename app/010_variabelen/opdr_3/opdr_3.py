7643c59e594aa313dbf13ed2bc9de4e44d6f7086
RobV2004 <97097512@st.deltion.nl>
2026-02-06 09:10:20 +0100

    Opdracht 3

--- app/010_variabelen/opdr_3/opdr_3.py
+++ app/010_variabelen/opdr_3/opdr_3.py
@@ -4,9 +4,6 @@
 
 # Hier komt je code...
 
-my_dict = {"naam": = willem  
-achternaam = van der broek  
-leeftijd = 23  
-favoriete kleur = blauw  }
+my_dict = {"naam": "willem", "achternaam": "van der broek", "leeftijd": 23, "favoriete kleur": "blauw"}
 
-print()
+print(my_dict)

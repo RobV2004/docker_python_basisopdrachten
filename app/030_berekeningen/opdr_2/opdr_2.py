a647302eecc5cb865ab93cde8e79f5c570fdeb5e
RobV2004 <97097512@st.deltion.nl>
2026-02-11 10:30:22 +0100

    Opdracht 8

--- app/030_berekeningen/opdr_2/opdr_2.py
+++ app/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,20 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 102
 
-print()
+f_van_c = (c * 9/5) + 32
+c_van_f = (f - 32) * 5/9
+
+print(f"{c} graden Celcius is gelijk aan {f_van_c} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {c_van_f} graden Celsius")
+
+c = 62.2
+f = 96
+
+f_van_c = (c * 9/5) +32
+c_van_f = (f - 32) * 5/9
+
+print(f"{c} graden Celsius is gelijk aan {f_van_c} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {c_van_f} graden Celsius")

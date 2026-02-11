045040d456992631356b38474d9bd479fb68ef1d
RobV2004 <97097512@st.deltion.nl>
2026-02-11 09:58:41 +0100

    Opdracht 7

--- app/030_berekeningen/opdr_1/opdr_1.py
+++ app/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,15 @@
 # Groep:
 
 # Hier komt je code...
+
+joe = 22
+bob = 18
+fred = 21
+
+totaal = joe + bob + fred
+gemiddelde = totaal / 3
+
+
+print("Dit weten we over joe, bob en fred:")
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = {totaal}")
+print(f"De gemiddelde leeftijd van deze boys is: {gemiddelde}")
